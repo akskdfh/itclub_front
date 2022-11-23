292c6dad2dcae4f482cfd3208df10adae9c28faa
vanya vorobev <ivanvorobev14042004@mail.ru>
2022-11-23 21:51:08 +0700

    create login request

--- src/main/actions.js
+++ src/main/actions.js
@@ -1,14 +1,34 @@
-function scrollTo(element) {
-    window.scroll({
-      left: 0, 
-      top: element.offsetTop, 
-      behavior: 'smooth'
-    })
-  }
+// function scrollTo(element) {
+//     window.scroll({
+//       left: 0, 
+//       top: element.offsetTop, 
+//       behavior: 'smooth'
+//     })
+// }
+
+// var button = document.querySelector('a');
+// var footer = document.querySelector('#team');
   
-  var button = document.querySelector('a');
-  var footer = document.querySelector('#team');
-  
-  button.addEventListener('click', () => {
-    scrollTo(footer);
+// button.addEventListener('click', () => {
+//     scrollTo(footer);
+// })
+
+async function makeRequestWithoutToken(url, callback) {
+  fetch(url, {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+            "login": "vanyavvorobev",
+            "password": "string"
+        }
+      )
   })
+  .then(response => response.json())
+  .then(response => callback(JSON.stringify(response)))
+}
+
+makeRequestWithoutToken("http://localhost:8080/api/auth/login/", (response) => {console.log(response)});
