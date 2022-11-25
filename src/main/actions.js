e076f59015933e13ae219c53a88d4295e9881f36
vanyavvorobev <vanya.vorobevv@gmail.com>
2022-11-25 16:40:23 +0700

    add card

--- src/main/actions.js
+++ src/main/actions.js
@@ -13,22 +13,45 @@
 //     scrollTo(footer);
 // })
 
-async function makeRequestWithoutToken(url, callback) {
-  fetch(url, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(
-        {
-            "login": "vanyavvorobev",
-            "password": "string"
-        }
-      )
-  })
-  .then(response => response.json())
-  .then(response => callback(JSON.stringify(response)))
+function createCard() {
+    let div = document.createElement("div");
+    div.classList.add("person");
+
+    let pInfo = document.createElement("p");
+    pInfo.classList.add("info");
+    let img = document.createElement("img");
+    img.classList.add("photo");
+    img.src = "../../resources/1640528610_2-abrakadabra-fun-p-serii-chelovek-na-avu-2.jpg";
+    img.alt = "аватарка"
+    img.align = "left";
+    pInfo.appendChild(img);
+
+    let h2 = document.createElement("h2");
+    let info = document.createTextNode("Ivan Ivanov")
+    h2.appendChild(info);
+    
+
+    let pPos = document.createElement("p");
+    pPos.classList.add("position");
+    let pos = document.createTextNode("frontend")
+    pPos.appendChild(pos);
+
+    let pDesc = document.createElement("p");
+    pDesc.classList.add("description");
+    let desc = document.createTextNode("bla bla bla o sebe hbbj ggggygy ytytyt ttyt t ttyty ")
+    pDesc.appendChild(desc);
+
+    div.appendChild(pInfo);
+    div.appendChild(h2);
+    div.appendChild(pPos);
+    div.appendChild(pDesc);
+
+    document.getElementById("team").appendChild(div);
 }
 
-makeRequestWithoutToken("http://localhost:8080/api/auth/login/", (response) => {console.log(response)});
+// <div class="person">
+//                 <p class="info"> 
+
+//                 <p class="position">frontend</p>
+//                 <p class="description">bla bla bla o sebe hbbj ggggygy ytytyt ttyt t ttyty </p>
+//             </div>
