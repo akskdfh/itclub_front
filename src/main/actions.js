e40f22326f8b2287d6e2449332d6f736456bfc4c
Elena Karelina <karelinae03@mail.ru>
2022-11-25 16:45:51 +0700

    delete person

--- src/main/actions.js
+++ src/main/actions.js
@@ -1,34 +1,4 @@
-// function scrollTo(element) {
-//     window.scroll({
-//       left: 0, 
-//       top: element.offsetTop, 
-//       behavior: 'smooth'
-//     })
-// }
+const tag = document.createElement("p");
+tag.innerHTML="SSFFDFDDSDG";
+document.body.append(tag);
 
-// var button = document.querySelector('a');
-// var footer = document.querySelector('#team');
-  
-// button.addEventListener('click', () => {
-//     scrollTo(footer);
-// })
-
-async function makeRequestWithoutToken(url, callback) {
-  fetch(url, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(
-        {
-            "login": "vanyavvorobev",
-            "password": "string"
-        }
-      )
-  })
-  .then(response => response.json())
-  .then(response => callback(JSON.stringify(response)))
-}
-
-makeRequestWithoutToken("http://localhost:8080/api/auth/login/", (response) => {console.log(response)});
